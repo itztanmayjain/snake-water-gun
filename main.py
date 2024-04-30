e480ac329ad3fe3d3cc31b812073628eb42786e9
Tanmay Jain <itztanmayjain@users.noreply.github.com>
2024-04-30 14:36:40 +0530

    main file updated

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import random
 
+choices = ['snake', 'water', 'gun']
+computer_choice = random.choice(choices)
+player_choice = input("Enter your choice (snake/water/gun): ").lower()
+print(f"Computer chooses: {computer_choice}")
+if player_choice == computer_choice:
+    print("It's a tie!")
+elif player_choice == 'snake':
+    if computer_choice == 'water':
+        print("You win!")
+    else:
+        print("Computer wins!")
+elif player_choice == 'water':
+    if computer_choice == 'gun':
+        print("You win!")
+    else:
+        print("Computer wins!")
+elif player_choice == 'gun':
+    if computer_choice == 'snake':
+        print("You win!")
+    else:
+        print("Computer wins!")
+else:
+    print("Invalid input! Please enter snake, water, or gun.")
